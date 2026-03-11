45752cdc273818ffebaf2d0def7c7883d1aea68e
AngelDCS23 <angeldcs24@gmail.com>
2026-03-11 18:03:41 +0100

    add stuns servers

--- resources/views/livewire/file-transfer.blade.php
+++ resources/views/livewire/file-transfer.blade.php
@@ -167,7 +167,27 @@ new class extends Component {
         },
 
         async initWebRTC() {
-            const config = { iceServers: [{ urls: 'stun:stun.l.google.com:19302' }] };
+            const config = { 
+                iceServers: [
+                    { urls: 'stun:stun.l.google.com:19302' },
+                    { 
+                        urls: 'turn:openrelay.metered.ca:80',
+                        username: 'openrelayproject',
+                        credential: 'openrelayproject'
+                    },
+                    { 
+                        urls: 'turn:openrelay.metered.ca:443',
+                        username: 'openrelayproject',
+                        credential: 'openrelayproject'
+                    },
+                    { 
+                        urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+                        username: 'openrelayproject',
+                        credential: 'openrelayproject'
+                    }
+                ] 
+            };
+            
             this.pc = new RTCPeerConnection(config);
 
             if (this.$wire.role === 'sender') {
